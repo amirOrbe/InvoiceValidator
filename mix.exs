d2abecfd8ccaf1369ec68b4d2b90c93e1aa85ffa
amirOrbe <amir.orbe@bunsan.io>
2022-03-25 11:44:01 -0600

    Coveralls and credo added

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule InvoiceValidator.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -23,7 +30,9 @@ defmodule InvoiceValidator.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:tzdata, "~> 1.1"}
+      {:tzdata, "~> 1.1"},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.6"}
     ]
   end
 end
--- test/invoice_validator_test.exs
+++ test/invoice_validator_test.exs
@@ -21,7 +21,9 @@ defmodule InvoiceValidatorTest do
   test "validate when emisor_date > 3" do
     emisor_date = DateTime.from_naive!(~N[2022-03-16 10:00:00], "Mexico/General")
     pac_date = DateTime.from_naive!(~N[2022-03-20 10:00:00], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
   end
 
   test "validate when emisor_date is equals than 5 mins ahead in time" do
@@ -33,37 +35,49 @@ defmodule InvoiceValidatorTest do
   test "validate when emisor_date is more than 5 mins ahead in time" do
     emisor_date = DateTime.from_naive!(~N[2022-03-20 10:06:00], "Mexico/General")
     pac_date = DateTime.from_naive!(~N[2022-03-20 10:00:00], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:error, "Invoice is more than 5 mins ahead in time"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice is more than 5 mins ahead in time"}
   end
 
   test "validate timezone in tijuana" do
     emisor_date = DateTime.from_naive!(~N[2022-03-20 10:06:00], "America/Tijuana")
     pac_date = DateTime.from_naive!(~N[2022-03-20 10:00:00], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:error, "Invoice is more than 5 mins ahead in time"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice is more than 5 mins ahead in time"}
   end
 
   test "locate America/tijuana 72hr before" do
     emisor_date = DateTime.from_naive!(~N[2022-03-20 13:06:31], "America/Tijuana")
     pac_date = DateTime.from_naive!(~N[2022-03-24 15:06:31], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
   end
 
   test "locate America/Sinaloa 72hr before" do
     emisor_date = DateTime.from_naive!(~N[2022-03-20 14:06:31], "America/Mazatlan")
     pac_date = DateTime.from_naive!(~N[2022-03-24 15:06:31], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
   end
 
   test "locate America/CDMX 72hr before" do
     emisor_date = DateTime.from_naive!(~N[2022-03-20 14:06:31], "America/Mexico_City")
     pac_date = DateTime.from_naive!(~N[2022-03-24 15:06:31], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
   end
 
   test "locate America/Qroo 72hr before" do
     emisor_date = DateTime.from_naive!(~N[2022-03-20 14:06:31], "America/Merida")
     pac_date = DateTime.from_naive!(~N[2022-03-24 15:06:31], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice was issued more than 72 hrs before received by the PAC"}
   end
 
   test "locate America/tijuana 72hr exact" do
@@ -93,48 +107,56 @@ defmodule InvoiceValidatorTest do
   test "locate America/Tijuana 5min exact" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 13:11:35], "America/Tijuana")
     pac_date = DateTime.from_naive!(~N[2022-03-23 13:11:35], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:ok}
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:ok}
   end
 
   test "locate America/Sinaloa 5min exact" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 14:11:35], "America/Mazatlan")
     pac_date = DateTime.from_naive!(~N[2022-03-23 14:11:35], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:ok}
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:ok}
   end
 
   test "locate America/CDMX 5min exact" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 15:11:35], "America/Mexico_City")
     pac_date = DateTime.from_naive!(~N[2022-03-23 15:11:35], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:ok}
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:ok}
   end
 
   test "locate America/Qroo 5min exact" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 16:11:35], "America/Merida")
     pac_date = DateTime.from_naive!(~N[2022-03-23 16:11:35], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:ok}
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) === {:ok}
   end
 
   test "locate America/Tijuana 5min after" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 13:11:35], "America/Tijuana")
     pac_date = DateTime.from_naive!(~N[2022-03-23 10:00:00], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:error, "Invoice is more than 5 mins ahead in time"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice is more than 5 mins ahead in time"}
   end
 
   test "locate America/Sinaloa 5min after" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 14:11:35], "America/Mazatlan")
     pac_date = DateTime.from_naive!(~N[2022-03-23 10:00:00], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:error, "Invoice is more than 5 mins ahead in time"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice is more than 5 mins ahead in time"}
   end
 
   test "locate America/CDMX 5min after" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 15:11:35], "America/Mexico_City")
     pac_date = DateTime.from_naive!(~N[2022-03-23 10:00:00], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:error, "Invoice is more than 5 mins ahead in time"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice is more than 5 mins ahead in time"}
   end
 
   test "locate America/Qroo 5min after" do
     emisor_date = DateTime.from_naive!(~N[2022-03-23 16:11:35], "America/Merida")
     pac_date = DateTime.from_naive!(~N[2022-03-23 10:00:00], "Mexico/General")
-    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===  {:error, "Invoice is more than 5 mins ahead in time"}
+
+    assert InvoiceValidator.validate_dates(emisor_date, pac_date) ===
+             {:error, "Invoice is more than 5 mins ahead in time"}
   end
 end
